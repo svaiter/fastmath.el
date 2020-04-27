74fc6e1142e74a1c653bdd1443be685d52fe3ad4
Samuel Vaiter <samuel.vaiter@gmail.com>
2020-04-27 11:35:48 +0200

    Add .dir-locals.el for bug report

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((bug-reference-url-format . "https://github.com/svaiter/github/issues/%s"))))
